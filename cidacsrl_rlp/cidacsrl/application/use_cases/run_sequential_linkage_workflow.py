740d6edb5ec90c0f8d7bf539057b39c402bada84
Rafael Romão <leafaromao@gmail.com>
2026-05-26 11:46:05 -0300

    feat: implement first steps to Linkage Workflow

--- cidacsrl_rlp/cidacsrl/application/use_cases/run_sequential_linkage_workflow.py
+++ cidacsrl_rlp/cidacsrl/application/use_cases/run_sequential_linkage_workflow.py
@@ -1,16 +1,20 @@
 from cidacsrl_rlp.cidacsrl.application.ports.outbound.data_reader_port import DataReaderPort
+from cidacsrl_rlp.cidacsrl.application.ports.outbound.get_candidates_port import GetCandidatesPort
+from cidacsrl_rlp.cidacsrl.application.ports.outbound.scoring_port import ScoringPort
 from cidacsrl_rlp.cidacsrl.domain.models.workflow import SequentialBlockingWorkflow
 
 
 class RunSequentialLinkageWorkflowUseCase:
-    def __init__(self, data_reader_port: DataReaderPort):        
-        self.data_reader = data_reader_port 
-
+    def __init__(self, data_reader_port: DataReaderPort, get_candidates_port: GetCandidatesPort, scoring_port: ScoringPort):        
+        self.data_reader = data_reader_port
+        self.get_candidates = get_candidates_port
+        self.scoring = scoring_port
+    
     def execute(self, config: SequentialBlockingWorkflow):
         df_source = self.data_reader.read_data()        
         for phase in config.blocking_phases:
             if phase.enabled:
-                self._run_phase(df_source, phase)
-    
-    def _run_phase(self, df_source, phase):
-        pass
+                df_candidates = self.get_candidates.get_candidates(df_source, phase)
+                df_scored_pairs = self.scoring.calculate_score(df_candidates, phase)
+
+        return df_scored_pairs
