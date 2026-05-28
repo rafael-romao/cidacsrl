e5aebabf2bd0e2903e2cae6bc1c27ddf5dc48c7f
Rafael Romão <leafaromao@gmail.com>
2026-05-28 10:04:11 -0300

    feat: now the data repository adapter get the data_format directly from enviroment config

--- cidacsrl_rlp/cidacsrl/infra/adapters/outbound/spark_data_repository_adapter.py
+++ cidacsrl_rlp/cidacsrl/infra/adapters/outbound/spark_data_repository_adapter.py
@@ -13,6 +13,8 @@ class SparkDataRepositoryAdapter(DataIngestionPort, DataPersistencePort, DataTra
         self.spark = spark_session        
         self.source_base_path = env_config.source_data_path
         self.output_base_path = env_config.output_base_path
+        self.source_format = env_config.source_format
+        self.output_format = env_config.output_format
 
     def _resolve_source_path(self, table_name: str) -> str:
         return os.path.join(self.source_base_path, table_name)
@@ -25,43 +27,45 @@ class SparkDataRepositoryAdapter(DataIngestionPort, DataPersistencePort, DataTra
         
         # Input
         try:            
+            logger.debug(f"Verificando acesso ao caminho de origem para a tabela '{source_table}' usando formato '{self.source_format}'.")
             physical_input_path = self._resolve_source_path(source_table)
-            self.spark.read.format("parquet").load(physical_input_path).limit(0).schema
+            self.spark.read.format(self.source_format).load(physical_input_path).limit(0).schema
         except Exception as e:
-            errors.append(f"Falha ao acessar o filesystem para LEITURA na tabela {source_table}: {str(e)}")
+            errors.append(f"Falha ao acessar o caminho {physical_input_path}: {str(e)}")
             
         # Output
         try:
+            logger.debug(f"Verificando acesso ao caminho de destino para o índice '{target_index}' usando formato '{self.output_format}'.")
             physical_output_path = os.path.join(self.output_base_path, target_index)
             test_df = self.spark.createDataFrame([("test",)], ["check"])
-            test_df.write.mode("overwrite").format("parquet").save(f"{physical_output_path}/.write_test")
+            test_df.write.mode("overwrite").format(self.output_format).save(f"{physical_output_path}/.write_test")
         except Exception as e:
-            errors.append(f"Falha ao acessar o filesystem para ESCRITA no destino {target_index}: {str(e)}")
+            errors.append(f"Falha ao acessar o caminho {physical_output_path} para ESCRITA: {str(e)}")
             
         return errors
 
-    def read_data(self, table_name: str, data_format: str, **kwargs) -> DataFrame:
+    def read_source_data(self, table_name: str, **kwargs) -> DataFrame:
         physical_path = self._resolve_source_path(table_name)
         logger.debug(f"Mapeando tabela lógica '{table_name}' para caminho: {physical_path}")        
         
-        return self.spark.read.format(data_format).load(physical_path)
+        return self.spark.read.format(self.source_format).load(physical_path)
 
-    def read_target_data(self, index_name: str, data_format: str, **kwargs) -> DataFrame:
+    def read_target_data(self, index_name: str, **kwargs) -> DataFrame:
         physical_path = self._resolve_source_path(index_name)
         logger.debug(f"Mapeando índice ES '{index_name}' para caminho: {physical_path}")        
         
-        return self.spark.read.format(data_format).load(physical_path)
+        return self.spark.read.format(self.source_format).load(physical_path)
     
-    def read_specific_partition(self, table_name: str, partition_expr: str, data_format: str, **kwargs) -> DataFrame:        
-        return self.read_data(table_name, data_format).filter(partition_expr)
+    def read_specific_partition(self, table_name: str, partition_expr: str, **kwargs) -> DataFrame:        
+        return self.read_source_data(table_name).filter(partition_expr)
 
     def get_partitioned_sample(self, table_name: str, fraction: float, seed: Optional[int] = None, **kwargs) -> DataFrame:
         """Gera uma amostragem estatística controlada da tabela especificada."""
-        return self.read_data(table_name, "parquet").sample(withReplacement=False, fraction=fraction, seed=seed)   
+        return self.read_source_data(table_name).sample(withReplacement=False, fraction=fraction, seed=seed)   
 
-    def write_data(self, data: DataFrame, table_name: str, data_format: str, **kwargs) -> None:        
+    def write_data(self, data: DataFrame, table_name: str, **kwargs) -> None:        
         physical_path = self._resolve_output_path(table_name)
-        data.write.mode("overwrite").format(data_format).save(physical_path)
+        data.write.mode("overwrite").format(self.output_format).save(physical_path)
 
     def exclude_records(self, primary_dataset: DataFrame, records_to_exclude: DataFrame, join_key: str) -> DataFrame:
         return primary_dataset.join(
