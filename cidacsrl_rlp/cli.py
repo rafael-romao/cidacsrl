c64981b08a16cd0ab62b7dcb1f4b0b7ad8a2d307
Rafael Romão <leafaromao@gmail.com>
2026-05-28 10:00:56 -0300

    chore: update runner to bootstrapper

--- cidacsrl_rlp/cli.py
+++ cidacsrl_rlp/cli.py
@@ -1,7 +1,7 @@
 import argparse
 from cidacsrl_rlp.shared.infra.spark.spark_factory import create_spark_session
 from cidacsrl_rlp.shared.infra.config_loader import load_yaml
-from cidacsrl.infra.adapters.inbound.linkage_runner import run_likage
+from cidacsrl_rlp.cidacsrl.infra.bootstrappers.linkage_bootstrapper import bootstrap_sequential_linkage
 from cidacsrl_cleaning.infra.adapters.inbound.cleaning_runner import run_cleaning
 from cidacsrl_indexing.infra.adapters.inbound.indexing_runner import run_indexing
 
@@ -18,7 +18,7 @@ def main():
 
     if args.pipeline == "linkage":
         spark = create_spark_session(app_name="CIDACS-RL Linkage Pipeline", **spark_config)
-        run_likage(pipeline_config, spark)
+        bootstrap_sequential_linkage(pipeline_config, spark)
     if args.pipeline == "cleaning":
         spark = create_spark_session(app_name="CIDACS-RL Cleaning Pipeline", **spark_config)
         run_cleaning(pipeline_config, spark)
